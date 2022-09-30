56f472ad371f6e8cccfc95e0ae08df6d01d61db0
kris <17947361+krisk248@users.noreply.github.com>
2022-10-01 01:15:16 +0530

    Updated with oss tools

--- manu.sh
+++ manu.sh
@@ -9,3 +9,9 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+
+# adding other oss tools for bug hunting
+
+go install github.com/lc/gau/v2/cmd/gau@latest
+
+go install github.com/OJ/gobuster/v3@latest
