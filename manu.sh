66952d0a269794d2f872050d330ce7692bc5c0e4
kris <17947361+krisk248@users.noreply.github.com>
2022-10-01 01:11:46 +0530

    Updated with projecy discovery tools

--- manu.sh
+++ manu.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 # this is manu script use it with caustion and for education puprpose only.
+
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
